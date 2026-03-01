5b68b0fd941590b67cd9ea56819b096d1fffb3ed
agpcarlos62-ai <agp.carlos62@gmail.com>
2026-03-01 15:53:08 -0300

    Update GallerySection.tsx

--- src/components/GallerySection.tsx
+++ src/components/GallerySection.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import gallery1 from "@/assets/ARTHUR.jpg";
+import gallery1 from "@/assets/gallery-1.jpg";
 import gallery2 from "@/assets/gallery-2.jpg";
 import gallery3 from "@/assets/gallery-3.jpg";
 import gallery4 from "@/assets/gallery-4.jpg";
